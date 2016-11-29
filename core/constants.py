b96e78e47a46afab52c0fa236f71ec10e857be1d
autolab <chris.powers@berkeley.edu>
2016-11-29 14:17:04 -0800

    removed unnecessary constants

--- core/constants.py
+++ core/constants.py
@@ -1,16 +1,6 @@
 """
-Global constants for the ALAN codebase
+Global constants for core module
 Author: Jeff Mahler
 """
 METERS_TO_MM = 1000.0
 MM_TO_METERS = 1.0 / METERS_TO_MM
-
-SUPPORTED_IMAGE_EXTS = ['.png', '.jpg']
-MAX_DEPTH = 1.5 # maximum expected depth reading
-MAX_IR = 65535 # maximum possible ir reading
-
-CAFFE_CNN_CHANNEL_SWAP = (2, 1, 0)
-CAFFE_CNN_CHANNELS = 3
-
-TF_EXTENSION = '.tf'
-INTR_EXTENSION = '.intr'
