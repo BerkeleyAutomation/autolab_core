867f408818754a27b6f8826153fd989dd25213a3
jeff <jmahler@berkeley.edu>
2017-01-03 15:10:17 -0800

    Readding docs

--- core/constants.py
+++
@@ -1,6 +0,0 @@
-"""
-Global constants for core module
-Author: Jeff Mahler
-"""
-METERS_TO_MM = 1000.0
-MM_TO_METERS = 1.0 / METERS_TO_MM
--- docs/gh_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Please provide the folder you want to deploy to GitHub Pages as an argument"
-  exit 1
-fi
-git push origin --delete gh-pages
-git subtree push --prefix $1 origin gh-pages
